67359503302377fa4e6a82a21fde6cc7c1088522
Nelson <nelsont87@gmail.com>
2016-03-14 11:36:51 -0700

    javascript scripting start

--- hackathon.js
+++ hackathon.js
@@ -1,33 +1,40 @@
 var add_to_page = function(item){
-  $("book1.name").text(book1.name);
-}
-
+  $("#book1 .name").text(item.name);
+  $("#book1 .category").text(item.category);
+  $("#book1 .picture").text(item.picture_url);
+  $("#book1 .price").text(item.price);
+  $("#book1 .selling_points").text(item.selling_points);
+};
 
 var book1 = {
-  name: ,
-  category: ,
+  name: "Redwall",
+  category: "Fantasy Adventure",
   picture_url:"" ,
-  price: ,
-  selling_points: [""]
+  price: 14.99,
+  selling_points: ["Compelling story line", " Mouth watering food descriptions", " Epic battles"]
 };
 var book2 = {
-  name: ,
-  category: ,
+  name: "Hardy Boys",
+  category: "Mystery",
   picture_url:"" ,
-  price: ,
-  selling_points: [""]
+  price: 12.95,
+  selling_points: ["Shrouded in mystery", " Fun to guess the answer"]
 };
-var album1 = {
-  name: ,
-  category: ,
-  picture_url:"" ,
-  price: ,
-  selling_points: [""]
-};
-var album2 = {
-  name: ,
-  category: ,
-  picture_url:"" ,
-  price: ,
-  selling_points: [""]
-};
+// var album1 = {
+//   name: ,
+//   category: ,
+//   picture_url:"" ,
+//   price: ,
+//   selling_points: [""]
+// };
+// var album2 = {
+//   name: ,
+//   category: ,
+//   picture_url:"" ,
+//   price: ,
+//   selling_points: [""]
+// };
+
+add_to_page(book1);
+//add_to_page(book2);
+
