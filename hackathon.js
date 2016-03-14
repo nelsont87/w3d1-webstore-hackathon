e73ad4b05e40cf920b3930f2d6ef452eaf364bbb
Nelson <nelsont87@gmail.com>
2016-03-14 10:14:50 -0700

    complete book 1 html page

---
+++ hackathon.js
@@ -0,0 +1,33 @@
+var add_to_page = function(item){
+	
+}
+
+
+var book1 = {
+  name: ,
+  category: ,
+  picture_url:"" ,
+  price: ,
+  selling_points: [""]
+};
+var book2 = {
+  name: ,
+  category: ,
+  picture_url:"" ,
+  price: ,
+  selling_points: [""]
+};
+var album1 = {
+  name: ,
+  category: ,
+  picture_url:"" ,
+  price: ,
+  selling_points: [""]
+};
+var album2 = {
+  name: ,
+  category: ,
+  picture_url:"" ,
+  price: ,
+  selling_points: [""]
+};
